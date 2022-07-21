3d99d5b3c7b298e679f6a63b546bd34bcd0bbd3f
IvanCruz39 <aguileracruzi@gmail.com>
2022-07-21 10:20:56 -0600

    Updates

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Producto from './components/Producto'
+import  Carrito from './components/Carrito'
+
+function App() {
+  
+  //Crear State de Listado de productos
+  const [productos, guardarProductos] = useState([
+    {id: 1, nombre: "Camisa React", precio: 50},
+    {id: 2, nombre: "Camisa Express", precio: 40},
+    {id: 3, nombre: "Camisa Electron", precio: 35},
+    {id: 4, nombre: "Camisa Vue", precio: 25},
+  ]);
+  
+  // Crear State de carrito de compras
+  const [carrito,agregarProducto]=useState([])
+  // Fecha Actual
+  const fecha = new Date().getFullYear()
+  
+  return (
+    <Fragment>
+      <Header 
+      titulo='Tienda virtual'
+      />
+
+{/* aqui se manda a llamar el array ,creando el boton que nos permite visualizar en consola que estamos comprando */}
+      <h1>Lista de Productos</h1>
+      {productos.map(producto =>(
+        <Producto 
+         producto ={producto}
+         key = {producto.id}
+         carrito={carrito}
+         agregarProducto={agregarProducto}
+         productos = {productos}
+         
+
+        />
+
+      ))}
+
+      <Carrito 
+      carrito = {carrito}
+      agregarProducto={agregarProducto}
+      />
+      <Footer
+      fecha={fecha} 
+      />
+    
+    </Fragment>
+  );
+}
+
+export default App
